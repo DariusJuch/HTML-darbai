b7945d6461073bd03f8b55cf721eb1750e28ff8f
Darius <juchevicd@gmail.com>
2024-12-12 13:33:29 +0200

    fixed

--- React/react12/src/components/ProfileCard.jsx
+++ React/react12/src/components/ProfileCard.jsx
@@ -1,39 +1,42 @@
 import React from "react";
-const PofileCard = (
-  username,
-  tag,
-  location,
-  avatar,
-  stats
-) => {
+import { getUsers } from "../helper/get";
+const PofileCard = () => {
+
+  const fetchData = async () => {
+    const data = await getUsers();
+    console.log(data);
+    
+  }
+
   return (
     <>
       <div className=" w-80 mx-auto my-8 p-6 bg-blue-50 rounded-lg shadow-lg text-center">
         <div className="w-24 h-24 mx-auto rounded-full overflow-hidden border-4 border-white">
           <img
-            src={avatar}
+            src=""
             alt="Profile"
             className=" w-full h-full object-cover"
           />
         </div>
-        <h2 className="mt-4 text-xl font-semibold text-gray-800">{username}</h2>
-        <p className=" text-gray-600">@{username}</p>
-        <p className="text-gray-600">{location}</p>
+        <h2 className="mt-4 text-xl font-semibold text-gray-800"></h2>
+        <p className=" text-gray-600">""</p>
+        <p className="text-gray-600"></p>
         <div className="flex justify-between mt-6 bg-pink-500 text-white py-3 px-4 rounded-lg">
           <div className=" text-center">
             <h3 className=" text-sm font-medium">Followers</h3>
-            <p className=" font-bold">{stats.follovers}</p>
+            <p className=" font-bold"></p>
           </div>
           <div className="w-px bg-white"></div>
           <div className=" text-center">
             <h3 className=" text-sm font-medium">Views</h3>
-            <p className=" font-bold">{stats.views}</p>
+            <p className=" font-bold"></p>
           </div>
           <div className="w-px bg-white"></div>
           <div className=" text-center">
             <h3 className=" text-sm font-medium">Likes</h3>
-            <p className=" font-bold">{stats.likes}</p>
+            <p className=" font-bold"></p>
           </div>
+          <button onChange={fetchData()}></button>
         </div>
       </div>
     </>
