52639d62aa7ed71f847f9e2e4bd64ff8651521c6
Darius <juchevicd@gmail.com>
2024-12-13 14:00:14 +0200

    end

--- React/user card/src/App.jsx
+++ React/user card/src/App.jsx
@@ -1,19 +1,59 @@
 import PofileCard from './components/ProfileCard'
 import './App.css'
 import { getUsers } from './helper/get';
-import { useState } from 'react';
-import { data } from 'autoprefixer';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [users, setUsers] = useState();
+  const [error, setError] = useState("");
 
+  const fetchData = async () => {
+    const data = await getUsers();
+    // console.log(data);
+    setUsers(data);
+    } 
 
+    useEffect(()=> {
+      fetchData();
+    }, [])
+  
+    if(!users){
+      return <p>loading...</p>
+    }
+    
 
+  console.log(users);
+
+  const {avatar, tag, location, stats, username } = users;
+
+  // console.log(avatar)
+  
+  
+  // Solution 1:
+  // return (
+  //   <>
+  //     <PofileCard user={users[0]} />
+  //   </>
+  // )
+
+  // Solution 2:
   return (
     <>
-      <PofileCard />
+    {
+      users.map(user=>
+        <PofileCard user={user} />
+      )
+    }
     </>
   )
+
+  // const array = [1,2,3]
+  // return (
+  //   <>
+  // {array}
+  //   </>
+  // )
 }
 
 export default App
--- React/user card/src/components/ProfileCard.jsx
+++ React/user card/src/components/ProfileCard.jsx
@@ -1,41 +1,36 @@
-import { getUsers } from "../helper/get";
-const PofileCard = () => {
 
-  const fetchData = async () => {
-    const data = await getUsers();
-    console.log(data);
-    
-  }
-
+const PofileCard = ( {user} ) => {
+  const {avatar, tag, location, stats, username } = user;
+  console.log(user)
   return (
     <>
       <div className=" w-80 mx-auto my-8 p-6 bg-blue-50 rounded-lg shadow-lg text-center">
         <div className="w-24 h-24 mx-auto rounded-full overflow-hidden border-4 border-white">
           <img
-            src=""
+            src={avatar}
             alt="Profile"
             className=" w-full h-full object-cover"
           />
         </div>
-        <h2 className="mt-4 text-xl font-semibold text-gray-800"></h2>
-        <p className=" text-gray-600">""</p>
-        <p className="text-gray-600"></p>
+        <h2 className="mt-4 text-xl font-semibold text-gray-800">{username}</h2>
+        <p className=" text-gray-600">@{tag}</p>
+        <p className="text-gray-600">{location}</p>
         <div className="flex justify-between mt-6 bg-pink-500 text-white py-3 px-4 rounded-lg">
           <div className=" text-center">
             <h3 className=" text-sm font-medium">Followers</h3>
-            <p className=" font-bold"></p>
+            <p className=" text-black">{stats.followers}</p>
           </div>
           <div className="w-px bg-white"></div>
           <div className=" text-center">
             <h3 className=" text-sm font-medium">Views</h3>
-            <p className=" font-bold"></p>
+            <p className=" text-black">{stats.views}</p>
           </div>
           <div className="w-px bg-white"></div>
           <div className=" text-center">
             <h3 className=" text-sm font-medium">Likes</h3>
-            <p className=" font-bold"></p>
+            <p className=" text-black">{stats.likes}</p>
           </div>
-          <button onChange={fetchData()}></button>
+
         </div>
       </div>
     </>
